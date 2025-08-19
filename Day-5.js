b5694e1f3db99b9a5eef0025bfc0a0e100782d70
Sania Ghare <116621825+gharesania@users.noreply.github.com>
2025-08-19 19:34:01 +0530

    Create Day-5.js

---
+++ Day-5.js
@@ -0,0 +1,22 @@
+function findLeaders(arr) 
+{
+    let n = arr.length;
+    let leaders = [];
+    
+    let maxFromRight = arr[n - 1];
+    leaders.push(maxFromRight);
+    
+    for (let i = n - 2; i >= 0; i--) 
+    {
+        if (arr[i] > maxFromRight) 
+        {
+            maxFromRight = arr[i];
+            leaders.push(maxFromRight);
+        }
+    }
+    
+    return leaders.reverse();
+}
+
+let arr = [16, 17, 4, 3, 5, 2];
+console.log(findLeaders(arr));
